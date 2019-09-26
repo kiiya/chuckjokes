c9ce3ab1e7d84b2addd1ff340d5418ed323006be
kiiya <kiiyaerick@gmail.com>
2019-09-26 16:41:46 +0300

    inital commit

---
+++ tests/JokeFactoryTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace kiiya\chuckjokes\tests;
+
+use kiiya\chuckjokes\JokeFactory;
+use PHPUnit\Framework\TestCase;
+
+class JokeFactoryTest extends TestCase
+{
+    /** @test */
+    public function it_returns_a_random_joke()
+    {
+        $jokes = new JokeFactory([
+            'This is a joke',
+        ]);
+        $joke = $jokes->getRandomJoke();
+
+        $this->assertSame('This is a joke', $joke);
+    }
+
+    /** @test */
+    public function it_returns_a_predefined_joke()
+    {
+
+        $chuckNorrisJokes = [
+            'Chuck Norris does not wear a condom. Because there is no such thing as protection from Chuck Norris.',
+            'Chuck Norris tears cure cancer. Too bad he has never cried.',
+            'Chuck Norris counted to infinity... Twice.',
+            'If you can see Chuck Norris, he can see you. If you can\'t see Chuck Norris you may be only seconds away from death.'
+        ];
+
+        $jokes = new JokeFactory();
+
+        $joke = $jokes->getRandomJoke();
+
+        $this->assertContains($joke, $chuckNorrisJokes);
+    }
+}
